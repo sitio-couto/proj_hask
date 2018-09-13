44899ddc001a6b01724f4d4217b3288027eaf765
Vinicius Couto Espindola <vinicius.c.e@hotmail.com>
2018-09-13 14:08:13 -0300

    TODO: Add wait time in graph build, remove from combEdge

--- proj_haskell.hs
+++ proj_haskell.hs
@@ -1,13 +1,10 @@
 import Data.List.Split
-data Edge  a = Edge (a, String, Float) deriving (Show, Read, Eq)
-data Graph a = Graph [(a, [Edge])] deriving (Show, Read, Eq)
-data Bus   a = Bus (String, Float)
 
 main = do
   text <- readFile "in0.txt"
   let contents = splitData $ lines text
-      bus = Bus (getBusData $ contents!!1)
-      graph = Graph (buildGraph $ contents!!0)
+      bus = getBusData $ contents!!1
+      graph = buildGraph $ contents!!0
       (start:[finish]) = splitOn " " $ head (contents!!2)
       in
         print graph
@@ -32,47 +29,37 @@ splitData' (x:xs) acc
   | (x /= "") = splitData' xs $ x:acc
   | otherwise = (reverse acc):splitData' xs []
 
--- topVertex _ [] acc = acc
--- topVertex o ((v,e):gs) acc
---   | (o == v) = [(v,e),acc:gs]
---   | otherwise = topVertex o gs ((v,e):acc)
-
-mergeBusPath :: Graph -> [Bus] -> Graph
-mergeBusPath g [] = g
-mergeBusPath g (b:bs) = foldVertex (mergeBusPath g bs) g b
-
-foldVertex :: Graph -> Graph -> [Bus] -> Graph
+-- TODO NOT Checked
 foldVertex [] g _ = g
-foldVertex (o:gs) g b = tracePath o (foldVertex gs g b) b
+foldVertex (gi:gs) g b = addPaths gi (foldVertex gs g b) b
 
-tracePath :: Graph -> Graph -> [Bus] -> Graph
-tracePath ((v,e):gs) g b
-  | (paths == (_,[]) = g
-  | otherwise = combEdges paths wt --TODO checkar o que fazer aki
-  where paths = seekPaths g e b (v,[]) [v]
-        (_,wt) = b
+--addPaths checked
+addPaths (v,e) g (l,wt)
+  | (length paths < 2) = g
+  | otherwise = foldr (\x acc -> addEdge v x acc) g $ combEdges paths wt
+  where paths = tracePaths g e l [] [v]
 
-seekPaths :: Graph -> [Edge] -> [Bus]
-seekPaths g pe b (o,p) visited
-  | test = seekPaths g (getE g $ head next) b (o,n:p) (head next):visited
-  | otherwise = (o,p)
+-- tracePaths checked
+tracePaths g pe b p visited
+  | test = tracePaths g (getE v g) b (p++next) (v:visited)
+  | otherwise = p
   where
-    test = (next /= [])&&(not $ elem (head next) visited)
-    next = filter (/(_,mode,_) -> mode == b!!0) pe
+    test = (next /= [])&&(not $ elem v visited)
+    (v,_,_) = head next
+    next = filter (\(_,mode,_) -> mode == b) pe
 
-combEdges :: (a,[Edge]) -> Float -> [Edge]
-combEdges (_,[]) _ = g
-combEdges (_,[_]) _ = g
-combEdges (o,(v,t,w):p) wt = combEdges' (tail p) [(joinPaths b $ head p)]
-                             where b = (v,t,w+(wt/2))
+-- combEdges Checked
+combEdges ((v,t,w):p) wt = combEdges' (tail p) [(joinEdges e0 $ head p)]
+  where e0 = (v,t,w+wt)
 
-combEdges' :: [Edge] -> [Edge]
+-- combEdges' Checked
 combEdges' [] acc = acc
-combEdges' p acc = combEdges' (tail p) $ (joinEdges (head acc) $ head p)):acc
+combEdges' p acc = combEdges' (tail p) $ (joinEdges (head acc) $ head p):acc
 
-joinEdges :: Edge -> Edge -> Edge
-joinEdges (ov,ot,ow) (v,t,w) = (v,ot++" "++ov++" "++t,ow+w)
+-- joinEdges Checked
+joinEdges (ov,ot,ow) (v,t,w) = (v,ot++" "++ov++" "++t,nw)
+  where nw = (fromIntegral $ floor ((ow+w)*10))/10
 
-getE :: Graph -> [Edge]
-getE g target = edges
-  where (_,edges) = head $ filter (/(v,e) -> v == target) g
+-- GetE checked
+getE target g = edges
+  where (_,edges) = head $ filter (\(v,e) -> v == target) g
